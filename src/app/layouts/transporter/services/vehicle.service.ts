1193f2acbdef0cd81fb81e1d2535b6b8a08cd333
Bhushan Gadekar <b.gadekar@crif.com>
2020-05-18 22:13:52 +0530

    added codde

--- src/app/layouts/transporter/services/vehicle.service.ts
+++ src/app/layouts/transporter/services/vehicle.service.ts
@@ -1,7 +1,7 @@
 import { Vehicle } from './../../../shared/models/vehicle';
 import { environment } from './../../../../environments/environment';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -15,6 +15,8 @@ export class VehicleService {
   ) { }
 
   saveVehicle(vehicle: Vehicle): Observable<any> {
-    return this.http.post<Vehicle>(this.baseUrl + 'vehicles', vehicle);
+    const headers = new HttpHeaders();
+    headers.append('Access-Control-Allow-Origin', '*');
+    return this.http.post<Vehicle>(this.baseUrl + 'vehicles', JSON.stringify(vehicle), { headers: headers });
   }
 }
