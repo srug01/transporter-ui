983fcb2842951134cab36302c125088a7ca12e36
utsav-hub <utsavprht06@gmail.com>
2020-05-31 19:19:53 +0530

    Welcome Page Changes

--- src/app/default/welcome/welcome.component.ts
+++ src/app/default/welcome/welcome.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
@@ -8,6 +9,12 @@ import { Component, OnInit } from '@angular/core';
 export class WelcomeComponent implements OnInit {
 
   pageTitle: String='welcome to admin dashboar';
+
+  tabLinks = [
+    { label: 'Customer', link: 'customer-tab' },
+    { label: 'Transorter', link: 'transporter-tab' },
+    { label: 'Driver', link: 'driver-tab' },
+  ];
 
   constructor() { }
 
