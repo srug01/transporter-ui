0c3fb1f40c830662485a10955027337ccbbf45c0
utsav-hub <utsavprht06@gmail.com>
2020-05-31 19:16:08 +0530

    Welcome Page changes

---
+++ src/app/default/welcome/login/login.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  public loginForm: FormGroup;
+  constructor(
+    private fb: FormBuilder,
+  ) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+    });
+  }
+
+  submitLoginForm(ev) {
+  }
+
+}
---
+++ src/app/default/welcome/welcome.module.ts
@@ -0,0 +1,53 @@
+
+
+import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { MatCardModule } from '@angular/material/card';
+import {MatTabsModule} from '@angular/material/tabs';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+
+import {
+  LoginComponent,
+} from './login/login.component';
+import { WelcomeComponent} from './welcome.component';
+
+export const routes: Routes = [
+  { path: '', component: WelcomeComponent ,
+  children: [
+  { path: 'customer-tab', component: LoginComponent },
+  { path: 'transporter-tab', component: LoginComponent },
+  { path: 'driver-tab', component: LoginComponent },
+  ]
+}
+];
+
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    WelcomeComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    MatCardModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forChild(routes)
+  ],
+  exports: [],
+})
+
+export class WelcomeModule { }
