42ce5e01732fcb444d3c743a87f6a9024c6814ba
Bhushan Gadekar <b.gadekar@crif.com>
2020-07-03 13:03:14 +0530

    added user profile

--- src/app/layouts/cfs/services/user-registration.service.ts
+++ src/app/layouts/cfs/services/user-registration.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 
@@ -12,32 +12,45 @@ import { environment } from './../../../../environments/environment';
 })
 export class UserRegistrationService {
   baseUrl = environment.baseUri;
+  public HttpUploadOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
+
   constructor(
     private http: HttpClient
   ) { }
 
 
-saveCfsUserRegistration(userregistration: Cfsuserregistration): Observable<any> {
-  console.log(userregistration);
-  delete userregistration.cfs_user_registration_syscode;
-  console.log(userregistration);
-  return this.http.post<Cfsuserregistration>(this.baseUrl + 'cfs-user-registrations', JSON.stringify(userregistration));
-}
+  saveCfsUserRegistration(userregistration: Cfsuserregistration): Observable<any> {
+    console.log(userregistration);
+    delete userregistration.cfs_user_registration_syscode;
+    console.log(userregistration);
+    return this.http.post<Cfsuserregistration>(
+      this.baseUrl + 'cfs-user-registrations',
+      JSON.stringify(userregistration),
+      this.HttpUploadOptions
+    );
+  }
 
-updateCfsUserRegistration(userregistration: Cfsuserregistration): Observable<any> {
-  return this.http.put<Cfsuserregistration>(this.baseUrl + 'cfs-user-registrations/'+ userregistration.cfs_user_registration_syscode,
-   JSON.stringify(userregistration));
-}
+  updateCfsUserRegistration(userregistration: Cfsuserregistration): Observable<any> {
+    return this.http.put<Cfsuserregistration>(
+      this.baseUrl + 'cfs-user-registrations/' + userregistration.cfs_user_registration_syscode,
+      JSON.stringify(userregistration),
+      this.HttpUploadOptions
+    );
+  }
 
-getAllCfsUserRegistration(): Observable<any> {
-  return this.http.get(this.baseUrl + 'cfs-user-registrations');
-}
+  getAllCfsUserRegistration(): Observable<any> {
+    return this.http.get(this.baseUrl + 'cfs-user-registrations', this.HttpUploadOptions);
+  }
 
-getCfsUserRegistrationById(id: number): Observable<any> {
-  return this.http.get(this.baseUrl + 'cfs-user-registrations/' + id);
-}
+  getCfsUserRegistrationById(id: number): Observable<any> {
+    return this.http.get(this.baseUrl + 'cfs-user-registrations/' + id, this.HttpUploadOptions);
+  }
 
-deleteCfsUserRegistrationById(id: number): Observable<any> {
-  return this.http.delete(this.baseUrl + 'cfs-user-registrations/' + id);
-}
+  deleteCfsUserRegistrationById(id: number): Observable<any> {
+    return this.http.delete(this.baseUrl + 'cfs-user-registrations/' + id, this.HttpUploadOptions);
+  }
 }
