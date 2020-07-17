82df1d34eb125c45c3943653f997075da096cdea
utsav-hub <utsavprht06@gmail.com>
2020-07-18 03:34:18 +0530

    weight master change

--- src/app/layouts/masters/weight/weight-master-list.component.ts
+++ src/app/layouts/masters/weight/weight-master-list.component.ts
@@ -1,9 +1,11 @@
 import { Weight } from './../../../shared/models/weight';
+import { ContainerMaster}  from './../../../shared/models/containerMaster';
 import { Component, OnInit } from '@angular/core';
 import { CdkTextareaAutosize } from '@angular/cdk/text-field';
 import { NgZone, ViewChild } from '@angular/core';
 import { take } from 'rxjs/operators';
 import { WeightService } from '../services/weight.service';
+import {ContainerService} from '../services/container.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
@@ -17,13 +19,15 @@ import { ConfirmDialogComponent } from 'src/app/shared/dialogs/confirm-dialog.co
 })
 export class WeightMasterListComponent implements OnInit {
   displayedColumns: string[] = [
-    'weightMasterId', 'weightDesc','container',
+    'weightMasterId', 'weightDesc', 'container',
     'isActive', 'action'
   ];
   @ViewChild('autosize') autosize: CdkTextareaAutosize;
   public weightMasters: Array<Weight> = [];
+  public containerMasters: Array<ContainerMaster> = [];
   constructor(
     private _weightService: WeightService,
+    private _containerService: ContainerService,
     private _snackBar: MatSnackBar,
     private _router: Router,
     public dialog: MatDialog
@@ -31,6 +35,8 @@ export class WeightMasterListComponent implements OnInit {
 
   ngOnInit(): void {
     this.getAllWeightMasters();
+    this.getAllContainerMasters();
+
   }
   openDialog(ev, weightId: number) {
     if (ev) {
@@ -55,6 +61,28 @@ export class WeightMasterListComponent implements OnInit {
       }
     );
   }
+
+  getAllContainerMasters() {
+    this._containerService.getAllContainerMasters().subscribe(
+      (containerMasters: ContainerMaster[]) => {
+        this.containerMasters = containerMasters;
+      },
+      (err) => {
+        console.log('could not fetch container masters');
+      }
+    );
+  }
+
+
+  getContainer(id): string {
+    for (let i = 0; i < this.containerMasters.length; i++) {
+      if (this.containerMasters[i].containerMasterId === id) {
+        return this.containerMasters[i].containerMasterName;
+      }
+    }
+  }
+
+
 
   deleteWeightById(weightId: number) {
     this._weightService.deleteWeightMasterById(weightId).subscribe(
