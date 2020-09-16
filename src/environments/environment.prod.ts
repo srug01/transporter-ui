a5e4a89f7d6bd100afdb1f83d5e75c91ae42a5ff
Bhushan Gadekar <bhushangadekar01@gmail.com>
2020-09-16 15:47:25 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUri : 'http://ec2-3-234-217-246.compute-1.amazonaws.com:3000/'
+  baseUri : 'http://ec2-3-235-184-23.compute-1.amazonaws.com:3000/'
 };
