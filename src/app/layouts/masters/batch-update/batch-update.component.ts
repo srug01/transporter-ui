2305b71a1111d6412d5f5b075a7d9138026439a7
Bhushan Gadekar <b.gadekar@crif.com>
2020-09-29 17:51:29 +0530

    fixed bug

--- src/app/layouts/masters/batch-update/batch-update.component.ts
+++ src/app/layouts/masters/batch-update/batch-update.component.ts
@@ -33,7 +33,7 @@ export class BatchUpdateComponent implements OnInit {
   @ViewChild('autosize') autosize: CdkTextareaAutosize;
   displayedColumns: string[] = [
     'containerMasterName', 'weightDesc', 'Rate', 'BidRate',
-    'OrderRate',  'Action'
+    'OrderRate', 'Action'
   ];
   public rateForm: FormGroup;
   public masterTypes: MasterType[] = [];
@@ -78,7 +78,7 @@ export class BatchUpdateComponent implements OnInit {
   getRateTableForCFS(masterType) {
     console.log(this.cfsId);
     console.log(this.selectedMasterType);
-    this._containerService.getAllContainersAndWeights(masterType.masterTypeId,this.cfsId).subscribe(
+    this._containerService.getAllContainersAndWeights(masterType.masterTypeId, this.cfsId).subscribe(
       (rateMasters) => {
         this.rateMasters = rateMasters;
       },
@@ -105,12 +105,11 @@ export class BatchUpdateComponent implements OnInit {
     this.getRateTableForCFS(this.selectedMasterType);
   }
 
-  updateRate(rateMaster: any)
-  {
+  updateRate(rateMaster: any) {
     console.log(rateMaster);
-    if(this.selectedMasterType.masterTypeId === 1) //CFS to Yard
-    {
-      this._cfsYardRate.cfsMasterId =  this.cfsId;
+    // CFS to Yard
+    if (this.selectedMasterType.masterTypeId === 1) {
+      this._cfsYardRate.cfsMasterId = this.cfsId;
       this._cfsYardRate.yardMasterId = rateMaster.yardMasterId;
       this._cfsYardRate.cfsYardRateMasterId = rateMaster.rateMasterId;
       this._cfsYardRate.containerMasterId = rateMaster.containerMasterId;
@@ -123,16 +122,14 @@ export class BatchUpdateComponent implements OnInit {
       if (this._cfsYardRate.cfsYardRateMasterId === 0) {
         this._cfsYardRate.createdBy = this.userId;
         this._cfsYardRate.createdOn = new Date();
-        this._cfsyardrateService.saveCfsYardRateMaster( this._cfsYardRate);
+        this._cfsyardrateService.saveCfsYardRateMaster(this._cfsYardRate);
       } else {
         this._cfsYardRate.modifiedBy = this.userId;
         this._cfsYardRate.modifiedOn = new Date();
-        this._cfsyardrateService.updateCfsYardRateMaster( this._cfsYardRate);
+        this._cfsyardrateService.updateCfsYardRateMaster(this._cfsYardRate);
       }
-    }
-    else if(this.selectedMasterType.masterTypeId === 3) //Yard to CFS
-    {
-      this._yardcfsrate.cfsMasterId =  this.cfsId;
+    } else if (this.selectedMasterType.masterTypeId === 3) { //Yard to CFS
+      this._yardcfsrate.cfsMasterId = this.cfsId;
       this._yardcfsrate.yardMasterId = rateMaster.yardMasterId;
       this._yardcfsrate.yardCfsRateMasterId = rateMaster.rateMasterId;
       this._yardcfsrate.containerMasterId = rateMaster.containerMasterId;
@@ -145,16 +142,14 @@ export class BatchUpdateComponent implements OnInit {
       if (this._yardcfsrate.yardCfsRateMasterId === 0) {
         this._yardcfsrate.createdBy = this.userId;
         this._yardcfsrate.createdOn = new Date();
-        this._yardcfsrateService.saveYardcfsrateMaster( this._yardcfsrate);
+        this._yardcfsrateService.saveYardcfsrateMaster(this._yardcfsrate);
       } else {
         this._yardcfsrate.modifiedBy = this.userId;
         this._yardcfsrate.modifiedOn = new Date().toString();
-        this._yardcfsrateService.updateYardcfsrateMaster( this._yardcfsrate);
+        this._yardcfsrateService.updateYardcfsrateMaster(this._yardcfsrate);
       }
-    }
-    else if(this.selectedMasterType.masterTypeId === 4) //Port to CFS
-    {
-      this._portcfsrate.cfsMasterId =  this.cfsId;
+    } else if (this.selectedMasterType.masterTypeId === 4) { //Port to CFS
+      this._portcfsrate.cfsMasterId = this.cfsId;
       this._portcfsrate.portCfsRateMasterId = rateMaster.rateMasterId;
       this._portcfsrate.containerMasterId = rateMaster.containerMasterId;
       this._portcfsrate.weightMasterId = rateMaster.weightMasterId;
@@ -166,16 +161,14 @@ export class BatchUpdateComponent implements OnInit {
       if (this._portcfsrate.portCfsRateMasterId === 0) {
         this._portcfsrate.createdBy = this.userId;
         this._portcfsrate.createdOn = new Date();
-        this._portcfsrateService.savePortCfsRateMaster( this._portcfsrate);
+        this._portcfsrateService.savePortCfsRateMaster(this._portcfsrate);
       } else {
         this._portcfsrate.modifiedBy = this.userId;
         this._portcfsrate.modifiedOn = new Date();
-        this._portcfsrateService.updatePortCfsRateMaster( this._portcfsrate);
+        this._portcfsrateService.updatePortCfsRateMaster(this._portcfsrate);
       }
-    }
-    else if(this.selectedMasterType.masterTypeId === 2) //CFS to Port
-    {
-      this._cfsportrate.cfsMasterId =  this.cfsId;
+    } else if (this.selectedMasterType.masterTypeId === 2) { // CFS to Port
+      this._cfsportrate.cfsMasterId = this.cfsId;
       this._cfsportrate.cfsPortRateMasterId = rateMaster.rateMasterId;
       this._cfsportrate.containerMasterId = rateMaster.containerMasterId;
       this._cfsportrate.weightMasterId = rateMaster.weightMasterId;
@@ -187,11 +180,11 @@ export class BatchUpdateComponent implements OnInit {
       if (this._cfsportrate.cfsPortRateMasterId === 0) {
         this._cfsportrate.createdBy = this.userId;
         this._cfsportrate.createdOn = new Date();
-        this._cfsportrateService.saveCfsRateMaster( this._cfsportrate);
+        this._cfsportrateService.saveCfsRateMaster(this._cfsportrate);
       } else {
         this._cfsportrate.modifiedBy = this.userId;
         this._cfsportrate.modifiedOn = new Date();
-        this._cfsportrateService.updateCfsRateMaster( this._cfsportrate);
+        this._cfsportrateService.updateCfsRateMaster(this._cfsportrate);
       }
     }
 
@@ -202,16 +195,16 @@ export class BatchUpdateComponent implements OnInit {
     if (ev) {
       ev.preventDefault();
     }
-   /*  if (this.portcfsrateForm.valid) {
-      const portcfsRate = this.transformCfsRateObj(this.portcfsrateForm.value);
-      if (!this.portcfsrateData) {
-         this.savePortCfsrateMaster(portcfsRate);
-      } else {
-        this.updatePortCfsrateMaster(portcfsRate);
-      }
-    } else {
-      this.openSnackBar('Invalid Form !', 'Please review all fields');
-    } */
+    /*  if (this.portcfsrateForm.valid) {
+       const portcfsRate = this.transformCfsRateObj(this.portcfsrateForm.value);
+       if (!this.portcfsrateData) {
+          this.savePortCfsrateMaster(portcfsRate);
+       } else {
+         this.updatePortCfsrateMaster(portcfsRate);
+       }
+     } else {
+       this.openSnackBar('Invalid Form !', 'Please review all fields');
+     } */
   }
 
   openUpdateDialog(ev, rateMaster: any) {
