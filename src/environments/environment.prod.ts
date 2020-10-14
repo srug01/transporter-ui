23cf38df0b3f59964b8929b6ccd4796e953a9e3b
utsav-hub <62434747+utsav-hub@users.noreply.github.com>
2020-10-14 18:31:01 +0530

    Instance change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUri : 'http://ec2-3-238-74-198.compute-1.amazonaws.com:3000/'
+  baseUri : 'http://ec2-3-234-240-85.compute-1.amazonaws.com:3000/'
 };
