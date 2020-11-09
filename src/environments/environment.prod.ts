e17490b2bf5eda56cfdb7041d7ee95a7ee137cfb
utsav-hub <utsavprht06@gmail.com>
2020-11-09 21:19:57 +0530

    aws changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUri : 'http://ec2-3-238-104-249.compute-1.amazonaws.com:3000/'
+  baseUri : 'http://ec2-34-204-197-210.compute-1.amazonaws.com:3000/'
 };
